caf7fbcb7030e1e45b757ea95ac666bf2bf38f0d
Abhay <abhayonlyone@gmail.com>
2021-05-18 09:20:40 +0530

    Updated disjoint set find to be iterative

--- data-structures/disjoint-set/disjoint-set.hpp
+++ data-structures/disjoint-set/disjoint-set.hpp
@@ -11,10 +11,26 @@ public:
   }
 
   size_t find_set(size_t vertex) {
+    std::vector<size_t> path_nodes;
+
+    while (vertex != _parent[vertex]) {
+      path_nodes.push_back(vertex);
+      vertex = _parent[vertex];
+    }
+
+    const int final_parent = vertex;
+    for (const auto &node : path_nodes) {
+      _parent[node] = final_parent; // Path Compression
+    }
+    /*
+     * recursive
     if (vertex == _parent[vertex]) {
       return vertex;
     }
     return _parent[vertex] = find_set(_parent[vertex]); // Path Compression
+    */
+
+    return final_parent;
   }
   void union_sets(size_t a, size_t b) {
     a = find_set(a);
