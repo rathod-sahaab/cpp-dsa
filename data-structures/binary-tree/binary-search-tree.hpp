5ee6d1123bb2245a921af0bc6f6ce4c437f94bd3
Abhay <abhayonlyone@gmail.com>
2021-06-10 02:03:43 +0530

    Added remove for binary search tree

--- data-structures/binary-tree/binary-search-tree.hpp
+++ data-structures/binary-tree/binary-search-tree.hpp
@@ -7,9 +7,19 @@
 template <typename T> class BinarySearchTree : public BinaryTree<T> {
 public:
   void insert(T val);
+  void remove(T val);
 
   bool binary_search(const T &val) const noexcept;
   BinaryNode<T> *binary_search_ptr(const T &val) const noexcept;
+
+private:
+  /**
+   * @param node pointer to node to be deleted
+   * @param parent pointer to parent of node to be deleted
+   * @param is_left_child if [node] is the left child of [parent]
+   */
+  void remove_helper(BinaryNode<T> *node, BinaryNode<T> *parent,
+                     bool is_left_child);
 };
 
 template <typename T> void BinarySearchTree<T>::insert(T val) {
@@ -37,6 +47,80 @@ template <typename T> void BinarySearchTree<T>::insert(T val) {
   }
 }
 
+template <typename T> void BinarySearchTree<T>::remove(T val) {
+  const auto root = BinaryTree<T>::_root;
+
+  BinaryNode<T> *curr = root, *parent = nullptr;
+  bool is_left_child;
+  while (curr) {
+    if (val == curr->value) {
+      // is_left_child is undefined for parent == nullptr i.e. element to remove
+      // is root element
+      return remove_helper(curr, parent, is_left_child);
+    }
+    parent = curr;
+    if (val < curr->value) {
+      curr = curr->left;
+      is_left_child = true;
+      continue;
+    }
+    curr = curr->right;
+    is_left_child = false;
+  }
+}
+
+template <typename T>
+void BinarySearchTree<T>::remove_helper(BinaryNode<T> *node,
+                                        BinaryNode<T> *parent,
+                                        bool is_left_child) {
+  if (not node->left and not node->right) {
+    // case A when node is leaf
+    if (parent) {
+      BinaryNode<T> *&parent_to_node =
+          is_left_child ? parent->left : parent->right;
+      parent_to_node = nullptr;
+    } else {
+      BinaryTree<T>::_root = nullptr;
+    }
+    delete node;
+  }
+  if (not node->left xor not node->right) {
+    if (parent) {
+      BinaryNode<T> *&parent_to_node =
+          is_left_child ? parent->left : parent->right;
+      parent_to_node = node->left ? node->left : node->right;
+    } else {
+      BinaryTree<T>::_root = node->left ? node->left : node->right;
+    }
+
+    delete node;
+  }
+
+  // inorder successor : left most child/smallest element of right subtree
+  BinaryNode<T> *is = node->right, *parent_is = node;
+
+  while (is->left) {
+    parent_is = is;
+    is = is->left;
+  }
+
+  if (parent_is == node) {
+    is->left = node->left;
+    is->right = nullptr;
+  } else {
+    parent_is->left = is->right;
+    is->left = node->left;
+    is->right = node->right;
+  }
+  delete node;
+  if (parent) {
+    BinaryNode<T> *&parent_to_node =
+        is_left_child ? parent->left : parent->right;
+    parent_to_node = is;
+  } else {
+    BinaryTree<T>::_root = is;
+  }
+}
 /// TODO: use custom comparator
 /// decltype(std::less<T>()) comparator = std::less<T>())
 template <typename T>
