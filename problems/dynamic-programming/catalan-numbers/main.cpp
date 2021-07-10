e72b745d2a49e2056f0286dac9ee4d7a480dc1ef
Abhay <abhayonlyone@gmail.com>
2021-07-11 01:39:33 +0530

    Added iterative solution of catalan numberws

--- problems/dynamic-programming/catalan-numbers/main.cpp
+++ problems/dynamic-programming/catalan-numbers/main.cpp
@@ -16,12 +16,28 @@ int nth_catalan_number(vector<int> &dp, int n) {
   return dp[n] = res;
 }
 
+int nth_catalan_number_iter(int n) {
+  vector<int> catalans(n + 1);
+
+  catalans[0] = 1;
+
+  for (int i = 1; i <= n; ++i) {
+    catalans[i] = 0;
+    for (int j = 0; j < i; ++j) {
+      catalans[i] += catalans[j] * catalans[i - 1 - j];
+    }
+  }
+
+  return catalans[n];
+}
+
 void solve() {
   int n;
   cin >> n;
 
   vector<int> dp(n + 1, -1);
-  cout << nth_catalan_number(dp, n) << std::endl;
+  cout << nth_catalan_number(dp, n) << " " << nth_catalan_number_iter(n)
+       << std::endl;
 }
 
 int main(int argc, char *argv[]) {
