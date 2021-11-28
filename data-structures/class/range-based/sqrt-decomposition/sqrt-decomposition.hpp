c32733732ffe63958224648a24f4bc0a9686c5c1
Abhay Raj Singh <abhayonlyone@gmail.com>
2021-11-28 13:20:50 +0530

    bug fixes and simplification

--- data-structures/class/range-based/sqrt-decomposition/sqrt-decomposition.hpp
+++ data-structures/class/range-based/sqrt-decomposition/sqrt-decomposition.hpp
@@ -9,14 +9,16 @@
 #include <iostream>
 #endif
 
-template <typename T, class Core> class SqrtDecomposition {
+template <typename T, class OperationPolicy> class SqrtDecomposition {
 public:
-  std::vector<T> _data, _decomp;
+  std::vector<T> _data;
+  std::vector<T> _decomp;
   int _block_size;
 
   // constructors, asssignment, destructor
   explicit SqrtDecomposition(const std::vector<T> &data)
-      : _data(data), _decomp(ceil(sqrt(data.size())), Core::identitiy()),
+      : _data(data),
+        _decomp(ceil(sqrt(data.size())), OperationPolicy::identitiy()),
         _block_size(::sqrt(data.size())) {
 
     int curr_block = -1;
@@ -25,11 +27,11 @@ public:
         curr_block++;
       }
       auto &d = _decomp[curr_block];
-      d = Core::operation(d, _data[i]);
+      d = OperationPolicy::operation(d, _data[i]);
     }
 
 #if ALL_DEBUG
-    std::cout << "Construction copleted.\n";
+    std::cout << "Construction completed.\n";
 
     for (const auto &elem : _data) {
       std::cout << elem << " ";
@@ -45,42 +47,42 @@ public:
   }
 
   T query(int l, int r) {
-    T result = Core::identitiy(); // identitiy
+    T result = OperationPolicy::identitiy(); // identitiy
     for (; l <= r and l % _block_size != 0; l++) {
       // individuals before a block
-      result = Core::operation(result, _data[l]);
+      result = OperationPolicy::operation(result, _data[l]);
     }
 
     for (int i = l / _block_size; l + _block_size <= r; ++i, l += _block_size) {
       // block representatives
-      result = Core::operation(result, _decomp[i]);
+      result = OperationPolicy::operation(result, _decomp[i]);
     }
 
     for (; l <= r; ++l) {
       // individuals after a block
-      result = Core::operation(result, _data[l]);
+      result = OperationPolicy::operation(result, _data[l]);
     }
     return result;
   }
 
   void update(int index, T value) {
     int representating_block = index / _block_size;
-    _decomp[representating_block] = Core::block_update_calc(
+    _decomp[representating_block] = OperationPolicy::block_update_calc(
         _decomp[representating_block], value, _data[index]);
     _data[index] = value;
   }
 };
 
 // interface
-template <typename T> class OperationCore {
+template <typename T> class DummyPolicy {
 public:
-  static T identitiy() = 0;
-  static T operation(const T &a, const T &b) = 0;
+  static T identitiy();
+  static T operation(const T &a, const T &b);
   static T block_update_calc(const T old_block_value, const T new_data_value,
-                             const T &old_data_value) = 0;
+                             const T &old_data_value);
 };
 
-template <typename T> class AdditionCore {
+template <typename T> class AdditionPolicy {
 public:
   static T identitiy() { return 0; }
   static T operation(const T &a, const T &b) { return a + b; }
@@ -90,7 +92,7 @@ public:
   }
 };
 
-template <typename T> class MultiplicationCore {
+template <typename T> class MultiplicationPolicy {
 public:
   static T identitiy() { return 1; }
   static T operation(const T &a, const T &b) { return a * b; }
@@ -100,7 +102,7 @@ public:
   }
 };
 
-template <typename T> class MinimumCore {
+template <typename T> class MinimumPolicy {
 public:
   static T identitiy() { return std::numeric_limits<T>::max(); }
   static T operation(const T &a, const T &b) { return std::min(a, b); }
@@ -110,7 +112,7 @@ public:
   }
 };
 
-template <typename T> class MaximumCore {
+template <typename T> class MaximumPolicy {
 public:
   static T identitiy() { return std::numeric_limits<T>::min(); }
   static T operation(const T &a, const T &b) { return std::max(a, b); }
@@ -121,24 +123,16 @@ public:
 };
 
 template <typename T>
-class AdditionSqrtDecomposition : public SqrtDecomposition<T, AdditionCore<T>> {
-  using SqrtDecomposition<T, AdditionCore<T>>::SqrtDecomposition;
-};
+using AdditionSqrtDecomposition = SqrtDecomposition<T, AdditionPolicy<T>>;
 
 template <typename T>
-class MultiplicationSqrtDecomposition
-    : public SqrtDecomposition<T, MultiplicationCore<T>> {
-  using SqrtDecomposition<T, MultiplicationCore<T>>::SqrtDecomposition;
-};
+using MultiplicationSqrtDecomposition =
+    SqrtDecomposition<T, MultiplicationPolicy<T>>;
 
 template <typename T>
-class MinimumSqrtDecomposition : public SqrtDecomposition<T, MinimumCore<T>> {
-  using SqrtDecomposition<T, MinimumCore<T>>::SqrtDecomposition;
-};
+using MinimumSqrtDecomposition = SqrtDecomposition<T, MinimumPolicy<T>>;
 
 template <typename T>
-class MaximumSqrtDecomposition : public SqrtDecomposition<T, MaximumCore<T>> {
-  using SqrtDecomposition<T, MaximumCore<T>>::SqrtDecomposition;
-};
+using MaximumSqrtDecomposition = SqrtDecomposition<T, MaximumPolicy<T>>;
 
 #endif // SQRT_DECOMPOSITION_HPP
